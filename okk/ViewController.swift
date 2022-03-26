3caffd78360c2a90797fad3a84ff2f91953bf34a
d <d>
2022-03-26 03:19:15 +0300

    Initial Commit

---
+++ okk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  okk
+//
+//  Created by furkan beşik on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
